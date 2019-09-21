1c8327a9ffd71eb3c751e71cac254a9da81a8bfb
alexeygree <alexnogreen@gmail.com>
2019-09-21 23:06:54 +0300

    Задание 2. Отрисовка облака/тени и текста вынесены в разные функции. Сортировка максимального значения вынесена в отдельную функцию. Цикл for для отрисоки гистограммы заменен на forEach.

--- js/stat.js
+++ js/stat.js
@@ -1,63 +1,68 @@
 'use strict';
 
-(function () {
-  window.renderStatistics = function (canvas, userNames, timeSpend) {
-    this.ctx = canvas;
-    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    this.ctx.fillRect(110, 20, 420, 270);
+window.renderStatistics = function (canvas, names, times) {
+  var ctx = canvas;
+  cloudDraw(canvas);
+  textDraw(canvas);
 
-    this.ctx.fillStyle = '#fff';
-    this.ctx.fillRect(100, 10, 420, 270);
+  // Максимальный результат
+  var barMaxTime = maxValue(times);
+  // Максимальный результат
 
-    this.ctx.fillStyle = '#000';
-    this.ctx.font = 'bold 16px PT Mono';
-    this.ctx.fillText('Ура вы победили!', 120, 40);
+  // Отрисовка гистограммы
+  var barMargin = 150;
+  names.forEach(function (item, i) {
+    var barPercent = Math.round(times[i] * 100 / barMaxTime);
+    var barHeight = Math.round(140 * barPercent / 100);
+    var barTimePosition = 240 - barHeight - 10;
+    var colorSaturation = Math.random().toFixed(1) * 100;
+    var barColor = item === 'Вы'
+      ? 'rgba(255, 0, 0, 1)'
+      : 'hsl(240, 100%, ' + colorSaturation + '%)';
 
-    this.ctx.fillStyle = '#000';
-    this.ctx.font = 'bold 16px PT Mono';
-    this.ctx.fillText('Список результатов:', 120, 60);
+    ctx.fillStyle = '#000';
+    ctx.font = 'bold 16px PT Mono';
+    ctx.fillText(item, barMargin, 260);
 
-    var names = [];
-    var times = [];
-    // Копируем элементы массивов
-    for (var i = 0; i < userNames.length; i++) {
-      names[i] = userNames[i];
-      times[i] = Math.round(timeSpend[i]);
-    }
-    // Копируем элементы массивов
+    ctx.fillStyle = barColor;
+    ctx.fillRect(barMargin, 240, 40, -barHeight);
 
-    // Максимальный результат
-    var gistMaxTime = 0;
-    for (var j = 0; j < names.length; j++) {
-      if (gistMaxTime < times[j]) {
-        gistMaxTime = times[j];
-      }
-    }
-    // Максимальный результат
+    ctx.fillStyle = '#000';
+    ctx.font = 'bold 16px PT Mono';
+    ctx.fillText(Math.round(times[i]), barMargin, barTimePosition);
+    barMargin += 90;
+  });
+  // Отрисовка гистограммы
+};
 
-    // Отрисовка гистограммы
-    var gistMargin = 150;
-    for (var p = 0; p < names.length; p++) {
-      var gistPercent = Math.round(times[p] * 100 / gistMaxTime);
-      var gistHeight = Math.round(140 * gistPercent / 100);
-      var gistTimePosition = 240 - gistHeight - 10;
-      var gistColor = Math.random().toFixed(1) * 100;
+// Отрисовка облака и тени
+var cloudDraw = function (canvas) {
+  var ctx = canvas;
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
 
-      this.ctx.fillStyle = '#000';
-      this.ctx.font = 'bold 16px PT Mono';
-      this.ctx.fillText(names[p], gistMargin, 260);
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+};
+// Отрисовка облака и тени
 
-      this.ctx.fillStyle = 'hsl(240, 100%, ' + gistColor + '%)';
-      if (names[p] === 'Вы') {
-        this.ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      }
-      this.ctx.fillRect(gistMargin, 240, 40, -gistHeight);
+// Отрисовка текста
+var textDraw = function (canvas) {
+  var ctx = canvas;
+  ctx.fillStyle = '#000';
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
 
-      this.ctx.fillStyle = '#000';
-      this.ctx.font = 'bold 16px PT Mono';
-      this.ctx.fillText(times[p], gistMargin, gistTimePosition);
-      gistMargin += 90;
-    }
-    // Отрисовка гистограммы
-  };
-})();
+  ctx.fillStyle = '#000';
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Список результатов:', 120, 60);
+};
+// Отрисовка текста
+
+// Максимальный результат
+var maxValue = function (value) {
+  return value.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+};
+// Максимальный результат
