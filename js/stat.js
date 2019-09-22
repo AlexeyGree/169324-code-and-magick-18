21e6f293d52977e352ed8a25db2e0110c1ddb632
alexeygree <alexnogreen@gmail.com>
2019-09-22 19:53:09 +0300

    Задание 2. Базовые параметры вынесены в объекты

--- js/stat.js
+++ js/stat.js
@@ -2,33 +2,33 @@
 
 window.renderStatistics = function (canvas, names, times) {
   var ctx = canvas;
-  cloudDraw(canvas);
-  textDraw(canvas);
+  cloudDraw(canvas, cloudParams);
+  textDraw(canvas, textParams);
 
   // Максимальный результат
   var barMaxTime = maxValue(times);
   // Максимальный результат
 
   // Отрисовка гистограммы
-  var barMargin = 150;
+  var barMargin = barParams.LEFTPOSITION;
   names.forEach(function (item, i) {
     var barPercent = Math.round(times[i] * 100 / barMaxTime);
-    var barHeight = Math.round(140 * barPercent / 100);
-    var barTimePosition = 240 - barHeight - 10;
-    var colorSaturation = Math.random().toFixed(1) * 100;
+    var barHeight = Math.round((barParams.MAXHEIGHT - barParams.GAP) * barPercent / 100);
+    var barTimePosition = barParams.BOTTOMPOSITION - barHeight - 10;
+    // var colorSaturation = Math.random().toFixed(1) * 100;
     var barColor = item === 'Вы'
-      ? 'rgba(255, 0, 0, 1)'
-      : 'hsl(240, 100%, ' + colorSaturation + '%)';
+      ? barParams.YOURCOLOR
+      : barParams.COMPETITORCOLOR;
 
-    ctx.fillStyle = '#000';
-    ctx.font = 'bold 16px PT Mono';
+    ctx.fillStyle = textParams.COLOR;
+    ctx.font = textParams.FONT;
     ctx.fillText(item, barMargin, 260);
 
     ctx.fillStyle = barColor;
-    ctx.fillRect(barMargin, 240, 40, -barHeight);
+    ctx.fillRect(barMargin, barParams.BOTTOMPOSITION, barParams.WIDTH, -barHeight);
 
-    ctx.fillStyle = '#000';
-    ctx.font = 'bold 16px PT Mono';
+    ctx.fillStyle = textParams.COLOR;
+    ctx.font = textParams.FONT;
     ctx.fillText(Math.round(times[i]), barMargin, barTimePosition);
     barMargin += 90;
   });
@@ -36,26 +36,26 @@ window.renderStatistics = function (canvas, names, times) {
 };
 
 // Отрисовка облака и тени
-var cloudDraw = function (canvas) {
+var cloudDraw = function (canvas, params) {
   var ctx = canvas;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(params.X + params.GAP, params.Y + params.GAP, params.WIDTH, params.HEIGHT);
 
   ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillRect(params.X, params.Y, params.WIDTH, params.HEIGHT);
 };
 // Отрисовка облака и тени
 
 // Отрисовка текста
-var textDraw = function (canvas) {
+var textDraw = function (canvas, params) {
   var ctx = canvas;
-  ctx.fillStyle = '#000';
-  ctx.font = 'bold 16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillStyle = params.COLOR;
+  ctx.font = params.FONT;
+  ctx.fillText('Ура вы победили!', params.LEFTPOSITION, params.TOPPOSITIONFIRST);
 
-  ctx.fillStyle = '#000';
-  ctx.font = 'bold 16px PT Mono';
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillStyle = params.COLOR;
+  ctx.font = params.FONT;
+  ctx.fillText('Список результатов:', params.LEFTPOSITION, params.TOPPOSITIONSECOND);
 };
 // Отрисовка текста
 
@@ -66,3 +66,30 @@ var maxValue = function (value) {
   });
 };
 // Максимальный результат
+
+var cloudParams = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  GAP: 10
+};
+
+var textParams = {
+  COLOR: '#000',
+  FONT: 'bold 16px PT Mono',
+  LEFTPOSITION: 120,
+  TOPPOSITIONFIRST: 40,
+  TOPPOSITIONSECOND: 60
+};
+
+var barParams = {
+  WIDTH: 40,
+  MAXHEIGHT: 150,
+  LEFTPOSITION: 150,
+  BOTTOMPOSITION: 240,
+  GAP: 10,
+  YOURCOLOR: 'rgba(255, 0, 0, 1)',
+  COLORSATURATION: Math.random().toFixed(1) * 100,
+  COMPETITORCOLOR: 'hsl(240, 100%, ' + barParams.COLORSATURATION + '%)'
+};
