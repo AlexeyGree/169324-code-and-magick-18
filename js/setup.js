8ce3fc5e43f3273e9f85b0923968aa81fabbe658
alexeygree <alexnogreen@gmail.com>
2019-10-08 11:06:45 +0300

    Задание 4. Добавлены обработчики событий, для открытия и закрытия окна, а также изменения цвета мага

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
+var setup = document.querySelector('.setup');
 var similarList = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizardParams = {
@@ -9,6 +9,17 @@ var wizardParams = {
   ROBE_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
 };
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupUserName = setup.querySelector('.setup-user-name');
+var setupFocusOn = false;
+var ESC_KEY = 27;
+var ENTER_KEY = 13;
+var wizardSetup = setup.querySelector('.setup-wizard');
+var wizardSetupCoat = wizardSetup.querySelector('.wizard-coat');
+var wizardSetupEyes = wizardSetup.querySelector('.wizard-eyes');
+var fireballSetup = setup.querySelector('.setup-fireball-wrap');
 
 var getRandomElement = function (arr) {
   return arr[Math.round(Math.random() * (arr.length - 1))];
@@ -52,5 +63,76 @@ var addWizards = function (wizardsArr) {
 };
 
 addWizards(wizards);
-userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+// Окно не закрывается при фокусе на элементе "Имя"
+setupUserName.addEventListener('focus', function () {
+  setupFocusOn = true;
+});
+
+setupUserName.addEventListener('blur', function () {
+  setupFocusOn = false;
+});
+
+var pressEscOnPopup = function (evt) {
+  if (evt.keyCode === ESC_KEY) {
+    if (!setupFocusOn) {
+      closePopup();
+    }
+  }
+};
+// Окно не закрывается при фокусе на элементе "Имя"
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', pressEscOnPopup);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', pressEscOnPopup);
+};
+
+// Открытие окна
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    openPopup();
+  }
+});
+// Открытие окна
+
+// Закрытие окна
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    closePopup();
+  }
+});
+// Закрытие окна
+
+// Установка цветов
+wizardSetupCoat.addEventListener('click', function () {
+  var randomColor = getRandomElement(wizardParams.ROBE_COLORS);
+  wizardSetupCoat.style.fill = randomColor;
+  setup.querySelector('input[name=coat-color]').value = randomColor;
+});
+
+wizardSetupEyes.addEventListener('click', function () {
+  var randomColor = getRandomElement(wizardParams.EYES_COLORS);
+  wizardSetupEyes.style.fill = randomColor;
+  setup.querySelector('input[name=eyes-color]').value = randomColor;
+});
+
+fireballSetup.addEventListener('click', function () {
+  var randomColor = getRandomElement(FIREBALL_COLORS);
+  fireballSetup.style.backgroundColor = randomColor;
+  fireballSetup.querySelector('input[name=fireball-color]').value = randomColor;
+});
+// Установка цветов
